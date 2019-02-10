e14f6005d0112b4af233b3915e95646ced042b02
Brian LeRoux <b@brian.io>
2019-02-10 14:52:37 -0800

    Revert "hbd"
    This reverts commit 6dd1791d45994648d142f5339b10e7bd9f5c2346.

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-var cssmin = require('cssmin')
+let css = require('css')
 /**
- * @architect/proxy-plugin-css-min
+ * @architect/proxy-plugin-css-urls
+ *
+ * prepends @import url paths with /staging or /production
+ *
+ * // input
+ * @import '/bar.css';
+ *
+ * // output
+ * @import '/staging/bar.css';
  *
  * @param Key - the requested S3 Key
  * @param File - the file contents {headers, body}
  * @returns File - the processed file contents {header, body}
  */
-module.exports = function css(Key, {headers, body}) {
+module.exports = function prefixImport(Key, {headers, body}) {
+  if (process.env.NODE_ENV === 'testing')
+    return {headers, body}
+  let prefix
+  if (process.env.NODE_ENV === 'staging')
+    prefix = '/staging'
+  if (process.env.NODE_ENV === 'production')
+    prefix = '/production'
+  let ast = css.parse(body)
+  ast.stylesheet.rules = ast.stylesheet.rules.map(rule=> {
+    if (rule && rule.type && rule.type === 'import') {
+      rule.import = `'${prefix}${rule.import.replace(/('|")/g, '')}'`
+    }
+    return rule
+  })
   return {
     headers,
-    body: cssmin(body)
+    body: css.stringify(ast)
   }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-# `@architect/proxy-plugin-css-min`
+# `@architect/proxy-plugin-css-urls`
+
+Prepend `@import` URLs with `/staging` or `/production`
+
+```css
+/* input */
+@import '/bar.css';
+
+/* output */
+@import '/staging/bar.css';
+```
