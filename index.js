6dd1791d45994648d142f5339b10e7bd9f5c2346
Brian LeRoux <b@brian.io>
2019-02-10 14:49:32 -0800

    hbd

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-let css = require('css')
+var cssmin = require('cssmin')
 /**
- * @architect/proxy-plugin-css-urls
- *
- * prepends @import url paths with /staging or /production
- *
- * // input
- * @import '/bar.css';
- *
- * // output
- * @import '/staging/bar.css';
+ * @architect/proxy-plugin-css-min
  *
  * @param Key - the requested S3 Key
  * @param File - the file contents {headers, body}
  * @returns File - the processed file contents {header, body}
  */
-module.exports = function prefixImport(Key, {headers, body}) {
-  if (process.env.NODE_ENV === 'testing')
-    return {headers, body}
-  let prefix
-  if (process.env.NODE_ENV === 'staging')
-    prefix = '/staging'
-  if (process.env.NODE_ENV === 'production')
-    prefix = '/production'
-  let ast = css.parse(body)
-  ast.stylesheet.rules = ast.stylesheet.rules.map(rule=> {
-    if (rule && rule.type && rule.type === 'import') {
-      rule.import = `'${prefix}${rule.import.replace(/('|")/g, '')}'`
-    }
-    return rule
-  })
+module.exports = function css(Key, {headers, body}) {
   return {
     headers,
-    body: css.stringify(ast)
+    body: cssmin(body)
   }
 }
--- readme.md
+++ readme.md
@@ -1,11 +1,1 @@
-# `@architect/proxy-plugin-css-urls`
-
-Prepend `@import` URLs with `/staging` or `/production`
-
-```css
-/* input */
-@import '/bar.css';
-
-/* output */
-@import '/staging/bar.css';
-```
+# `@architect/proxy-plugin-css-min`
